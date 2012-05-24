a83315e8c38f6677e68410a7e099c6b51008bf7f
Excolo <nikolaj@excolo.dk>
2012-05-24 10:53:29 +0200

    fsefsf

---
+++ TACO/TACO.Model/Domain/IndoorPOI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excolo.Architecture.Core.Domain;
+
+namespace TACO.Model.Domain
+{
+    public class IndoorPOI : Entity
+    {
+        public virtual string IndoorPOIName { get; set; }
+        public virtual string IndoorPOIDescription { get; set; }
+        public virtual IEnumerable<Text> Texts { get; set; }
+        public virtual IEnumerable<WifiMeassure> WifiMeassures { get; set; }
+    }
+}
